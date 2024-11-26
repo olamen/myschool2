6592fdc6d0e0c8d42ba3b8db3352a9d7a01bdeba
olame <olame@live.fr>
2024-11-26 16:08:07 +0000

    makemigrations

--- reporting/models.py
+++ reporting/models.py
@@ -5,7 +5,7 @@ from students.models import Composition, SessionYearModel, Student
 class ReportCard(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="report_cards Reporting Card")
     exam = models.ForeignKey(Composition, on_delete=models.CASCADE, related_name="report_cards Reporting Exa")
-    sessionyear = models.ForeignKey(SessionYearModel, on_delete=models.SET_NULL)
+    sessionyear = models.ForeignKey(SessionYearModel, on_delete=models.CASCADE)
     total_score = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
     average_score = models.DecimalField(max_digits=4, decimal_places=2, default=0.0)
     rank = models.PositiveIntegerField(null=True, blank=True)
