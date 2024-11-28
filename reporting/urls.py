4b1b8d8bd8855aeb7fc8059ff0f2b3757f84c85d
olame <olame@live.fr>
2024-11-28 22:48:21 +0000

    reporting

--- reporting/urls.py
+++ reporting/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import exam_list, report_card_pdf, exam_detail , edit_exam
+from .views import exam_list, report_card_pdf, 
 
 urlpatterns = [
     path("exams/", exam_list, name="exam_list"),
-    path("exams/<int:exam_id>/", exam_detail, name="exam_detail"),
-    path("exams/<int:exam_id>/edit/", edit_exam, name="edit_exam"),
     path("report_card/<int:student_id>/<int:exam_id>/", report_card_pdf, name="report_card_pdf"),
 ]
