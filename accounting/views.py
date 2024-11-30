c056e6f3af40b1f1bda591ba48c174d402e41f57
olame <olame@live.fr>
2024-11-30 01:36:29 +0000

    b2911-1

--- accounting/views.py
+++ accounting/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 
 from Auth.models import CustomUser
 from students.models import Classe, Parent
-from .models import  Expense, Fee
+from .models import  Expense, Fee, Payment
 from accounting import models
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
