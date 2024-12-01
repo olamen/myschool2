43bb6caf4d75a5d425769a3c55a7523adce8b5e9
olame <olame@live.fr>
2024-12-01 15:25:26 +0000

    b0112

--- accounting/urls.py
+++ accounting/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('student-fees/',student_fee_list, name='student_fee_list'),
     path('student-fees/add/',add_student_fee, name='add_student_fee'),
     path('student-fees/edit/<int:fee_id>/', edit_student_fee, name='edit_student_fee'),
+    path('student-fees/archive/<int:fee_id>/', archive_student_fee, name='archive_student_fee'),
     path('student-fees/delete/<int:fee_id>/', delete_student_fee, name='delete_student_fee'),
 
 
