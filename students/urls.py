605ad998428870cf768b387c737d0a175a29b7b2
olame <olame@live.fr>
2024-12-02 16:45:21 +0000

    bbb

--- students/urls.py
+++ students/urls.py
@@ -9,7 +9,7 @@ from .views_composition import exam_detail, edit_exam
 from .views_parent import parent_create, parent_detail, parent_list
 
 from .views2 import BulkUploadStudentsView, GenerateExcelTemplateView, ListStudentPDFView
-from .views import AppConfigViewSet, CompositionViewSet, HomeworkViewSet, StudentViewSet, SubjectViewSet, TeacherViewSet, ClassViewSet, SessionYearViewSet, AttendanceViewSet, indexview,update_student, get_classes, grades_list, add_grade, update_grade
+from .views import AppConfigViewSet, CompositionViewSet, HomeworkViewSet, StudentViewSet, SubjectViewSet, TeacherViewSet, ClassViewSet, SessionYearViewSet, AttendanceViewSet, indexview, student_fees_by_month,update_student, get_classes, grades_list, add_grade, update_grade
 
 router = DefaultRouter()
 router.register(r'session-years', SessionYearViewSet)
