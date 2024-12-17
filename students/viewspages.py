878af0fe1b2846d12635c5650e61628b4124741a
olame <olame@live.fr>
2024-12-17 01:00:20 +0000

    b0716

--- students/viewspages.py
+++ students/viewspages.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from accounting.models import CashRegister
+
 @login_required
 def dashs(request):
     """Dashboard for Admins."""
@@ -9,4 +11,6 @@ def dashs(request):
 @login_required
 def dashf(request):
     """Dashboard for Adminf."""
+    cash_register = CashRegister.objects.get(user=request.user, is_open=True)
+
     return render(request, 'dash/dashf.html')
