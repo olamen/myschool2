75dbb9b7c547b457d9593884a683894d05a3bd64
olame <olame@live.fr>
2025-01-04 07:46:57 +0000

    b2512

--- reporting/urls.py
+++ reporting/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import exam_list, report_card_pdf,afficher_notes_devoir,afficher_notes_exam
-from .views_print import print_notes
+from .views_print import print_notes,rep
 from .views_excel import export_notes_to_excel
 from .views_bulletin import *
 
@@ -12,7 +12,7 @@ urlpatterns = [
     path('notes_exam/', afficher_notes_exam, name='afficher_notes_exams'),
     path('notes/export/', export_notes_to_excel, name='export_notes_to_excel'),
     path('notes/print/', print_notes, name='print_notes'),
-    path('notes/rep/', print_notes, name='rep'),
+    path('notes/rep/', rep, name='rep'),
     path('buletin_card/<int:student_id>/<int:trimestre_id>/', generate_report_card, name='generate_report_card'),
     path('class_report_cards/<int:classe_id>/<int:trimestre_id>/', generate_class_report_cards, name='generate_class_report_cards'),
     path('report_card/pdf/<int:student_id>/<int:trimestre_id>/', generate_pdf_report_card, name='generate_pdf_report_card'),
