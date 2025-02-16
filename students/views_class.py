3d4d47baca654f924c2a4252a16c6d71b870932a
olame <olame@live.fr>
2025-02-16 21:56:30 +0000

    0302

--- students/views_class.py
+++ students/views_class.py
@@ -7,32 +7,6 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Classe
 
-def class_create(request):
-    """
-    View to create a new class.
-    """
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        monthly_salary_fee = request.POST.get('monthly_salary_fee')
-
-        try:
-            # Try to create the class
-            new_class = Classe.objects.create(name=name, monthly_salary_fee=monthly_salary_fee)
-            messages.success(request, f"Classe '{new_class.name}' créée avec succès !")
-            return redirect('class_list')
-        except IntegrityError:
-            # Handle duplicate class name error
-            messages.error(request, f"Une classe avec le nom '{name}' existe déjà. Veuillez choisir un autre nom.")
-
-    return render(request, 'classes/class_form.html')
-
-def class_list(request):
-    """
-    View to list all active classes.
-    """
-    # Fetch all active classes
-    classes = Classe.objects.filter(is_active=True)
-    return render(request, 'classes/class_list.html', {'classes': classes})
 
 def class_archive(request, class_id):
     """
@@ -44,24 +18,6 @@ def class_archive(request, class_id):
     messages.success(request, f"La classe '{cls.name}' a été archivée avec succès.")
     return redirect('class_list')
 
-def class_update(request, class_id):
-    """
-    View to update an existing class.
-    """
-    cls = get_object_or_404(Classe, id=class_id)  # Fetch the class to update
-
-    if request.method == 'POST':
-        # Get data from the form
-        cls.name = request.POST.get('name')
-        cls.monthly_salary_fee = request.POST.get('monthly_salary_fee')
-
-        # Save the updated class
-        cls.save()
-        messages.success(request, f"La classe '{cls.name}' a été mise à jour avec succès.")
-        return redirect('class_list')  # Redirect to the class list
-
-    # Render the form with existing class data
-    return render(request, 'classes/class_form.html', {'cls': cls})
 
 def class_archived_list(request):
     """
@@ -97,6 +53,7 @@ def create_or_update_classe(request, classe_id=None):
             form = ClasseForm()
     
     return render(request, 'classes/create_or_update_classe.html', {'form': form, 'classe': classe})
+
 @login_required
 def list_classes(request):
     classes = Classe.objects.all()
