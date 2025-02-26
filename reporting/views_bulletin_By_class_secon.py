f3f101a97f8999a9d31185cb3f5205cab39b37ea
olame <olame@live.fr>
2025-02-26 02:08:33 +0000

    0302

--- reporting/views_bulletin_By_class_secon.py
+++ reporting/views_bulletin_By_class_secon.py
@@ -83,23 +83,28 @@ def generate_class_report_pdf(request, sessionyear_id, class_id):
     for rank, (student, general_avg) in enumerate(students_with_avg, start=1):
         # Left side: School Name and Year
         pdf.setFont("Helvetica", 16)
-        pdf.drawString( height - 50, "École XYZ - Bulletin Annuel")
-        pdf.drawString( height - 90, f"Année Scolaire : {session_year.name}")
+        pdf.drawString(50, height - 50, "École XYZ - Bulletin Annuel")
+        pdf.setFont("Helvetica", 14)
+        pdf.drawString(50, height - 90, f"Année Scolaire : {session_year.name}")
 
         # Center: Logo
         pdf.drawImage(400,LOGO_PATH, 50, height - 100, width=150, height=100)  
 
         # Right side: Custom text (Top Right)
         pdf.setFont("Amiri", 24)
-        pdf.drawRightString(width - 50, height - 50, fix_arabic_text("مدرسة الإمتياز"))  # Right-aligned Arabic text
+        pdf.drawRightString(width - 50, height - 50, fix_arabic_text("مدرسة الإمتياز"))
+        pdf.setFont("Amiri", 20)
+        pdf.drawRightString(width - 50, height - 70, fix_arabic_text("السنة  الدراسية"))
+        pdf.drawString(width - 90, height - 70, f"Année Scolaire : {session_year.name}")
+        # Right-aligned Arabic text
         pdf.setFont("Amiri", 16)
-        pdf.drawString(width - 200, height - 70, f"Date : {date.today().strftime('%d/%m/%Y')}")        
-        pdf.drawString(width - 200, height - 90, "N° Ref: 123456")
+        pdf.drawString(width - 200, height - 90, f"Date : {date.today().strftime('%d/%m/%Y')}")        
+        pdf.drawString(width - 200, height - 110, "N° Ref: 123456")
         # Student Information
-        pdf.setFont("Helvetica-Bold", 14)
-        pdf.drawString(50, height - 140, f"Nom de l'élève : {student.first_name} {student.last_name}")
-        pdf.drawString(50, height - 160, f"Classe : {student.student_class.name}")
-        pdf.drawString(50, height - 180, f"Rang : {rank}")
+        pdf.setFont("Helvetica-Bold", 13)
+        pdf.drawString(50, height - 120, f"Nom de l'élève : {student.first_name} {student.last_name}")
+        pdf.drawString(50, height - 140, f"Classe : {student.student_class.name}")
+        pdf.drawString(50, height - 160, f"Rang : {rank}")
 
         y_position = height - 180
 
