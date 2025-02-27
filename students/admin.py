38e4115511f324ee9a5a0d46c06f10686a8d85c8
olame <olame@live.fr>
2025-02-27 12:42:32 +0000

    2702

--- students/admin.py
+++ students/admin.py
@@ -27,9 +27,23 @@ class ParentAdmin(admin.ModelAdmin):
     search_fields = ('first_name', 'last_name', 'email', 'phone_number')
     filter_horizontal = ('children',)  # Enable easier management of linked students
 
+
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    list_display = ('name', 'subject', 'salary', 'salary_type', 'is_active')
+    list_display = ('name', 'nni', 'salary', 'salary_type', 'enrollment_date', 'is_active', 'telephone', 'get_subjects')
     list_filter = ('salary_type', 'is_active')
-    search_fields = ('name', 'subject')
+    search_fields = ('name', 'nni', 'telephone')
     ordering = ('name',)
+
+    def get_subjects(self, obj):
+        return ", ".join([subject.name for subject in obj.subject.all()])
+    get_subjects.short_description = 'Subjects'
+
+    def search_by_subject(self, queryset, name, value):
+        return queryset.filter(subject__name__icontains=value)
+    
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        if search_term:
+            queryset |= self.model.objects.filter(subject__name__icontains=search_term)
+        return queryset, use_distinct
