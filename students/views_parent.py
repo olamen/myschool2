9def4ce1c2e6df531732c20bcd781f483aeda7a1
olame <olame@live.fr>
2025-02-28 12:31:41 +0000

    2702

--- students/views_parent.py
+++ students/views_parent.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+
+from Auth.models import RoleChoices
 from .models import Parent, Student
 
 def parent_list(request):
@@ -12,9 +14,15 @@ def parent_list_card(request):
     parents = Parent.objects.all()
     return render(request, 'students/parent_list_card.html', {'parents': parents})
 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Parent, Student, CustomUser
+from .forms import ParentForm  # Use a custom form for input validation
+
 def parent_create(request):
     """View to create a new parent and link students."""
     if request.method == 'POST':
+        # Collect form data
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
@@ -24,7 +32,18 @@ def parent_create(request):
         children_ids = request.POST.getlist('children')  # List of student IDs
         photo = request.FILES.get('photo')  # Handle uploaded photo
 
-        # Create a new parent
+        # Create a user for the parent
+        user = CustomUser.objects.create_user(
+            username=nni,
+            password='defaultpassword',  # Replace this with a secure password
+            first_name=first_name,
+            email=email,
+        )
+        user.is_approved = True
+        user.role = RoleChoices.PARENT_OR_STUDENT
+        user.save()
+
+        # Create a new parent linked to the user
         parent = Parent.objects.create(
             first_name=first_name,
             last_name=last_name,
@@ -32,7 +51,8 @@ def parent_create(request):
             email=email,
             phone_number=phone_number,
             address=address,
-            photo=photo  # Save the uploaded photo
+            photo=photo,
+            user=user  # Link the parent to the created user
         )
         parent.children.set(Student.objects.filter(id__in=children_ids))
         parent.save()
@@ -42,10 +62,8 @@ def parent_create(request):
 
     # Get only students who are not already associated with a parent
     students = Student.objects.exclude(parents__isnull=False)
-
-    # allow student to have two parent
-    #students = Student.objects.annotate(parent_count=Count('parents')).filter(parent_count__lt=2)
     return render(request, 'students/parent_form.html', {'students': students})
+
 
 def parent_detail(request, parent_id):
     """View to display details about a specific parent."""
