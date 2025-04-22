639fd0f0489142569cff3ca45b78bdfb97109dd5
olame <olame@live.fr>
2025-04-22 16:57:14 +0000

    new by olame30

--- students/viewspages.py
+++ students/viewspages.py
@@ -1,5 +1,6 @@
+from pyexpat.errors import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from accounting.models import CashRegister
 
@@ -13,9 +14,11 @@ def dashs(request):
 def dashf(request):
     """Dashboard for Adminf."""
 
-    cash_register = CashRegister.objects.get(user=request.user )
-    context = {
-        'cash_register' : cash_register
-    }
+    cash_register = CashRegister.objects.filter(user=request.user).first()
 
-    return render(request, 'dash/dashf.html',context)
+    if not cash_register:
+        messages.error(request, "Aucun registre de caisse trouvé pour cet utilisateur.")
+        return redirect('some_other_page')  # Redirect to a fallback page
+
+    context = {'cash_register': cash_register}
+    return render(request, 'dash/dashf.html', context)
